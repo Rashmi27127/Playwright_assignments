d01a1d0a1fd4e5658717f359b7c6bbb8e9c90875
RASHMI27123 <112807985+RASHMI27123@users.noreply.github.com>
2026-09-21 03:41:18 +0530

    Add factorial and Fibonacci TypeScript assignments

---
+++ Day3/fibonacci.ts
@@ -0,0 +1,24 @@
+
+function fibonacci(n: number): number {
+
+    // First two Fibonacci numbers
+    let a = 0;
+    let b = 1;
+
+    // Calculate Fibonacci number
+    for (let i = 0; i < n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return a;
+}
+
+
+// Example calls
+console.log(fibonacci(0));  // 0
+console.log(fibonacci(1));  // 1
+console.log(fibonacci(2));  // 1
+console.log(fibonacci(5));  // 5
+console.log(fibonacci(10)); // 55
